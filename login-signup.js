66eb77145e2c24f45101363470d4fee820f8c438
MELATONIN99 <sindogfood@naver.com>
2024-06-19 12:19:30 +0900

    refactor. 로그인/회원가입 form 유효성 검사 개선
    * 복잡성을 줄이기 위해 유효성 검사 기능을 리팩토링하였습니다.
    리팩토링 결과 :코드 143줄 -> 111줄 22.38%감소, 기능성은 유지됨.
    
    * 정규 표현식을 대문자 상수화 시켰습니다.
    
    * 클래스명이 태그명과 겹치던 문제를 수정하였습니다.
    
    * 오타를 수정하였습니다.

--- login-signup.js
+++ login-signup.js
@@ -1,107 +1,78 @@
-const $input = document.getElementsByClassName('input');
+const $input = document.getElementsByClassName('input-text');
 const $email = document.getElementById('email');
-const $name = document.getElementById('name');
 const $password = document.getElementById('password');
 const $checkPassword = document.getElementById('checkPassword');
 const $loginBtn = document.getElementsByClassName('login-btn');
-const $form = document.getElementsByClassName('form');
-const $signup = document.getElementsByClassName('signup');
-const $login = document.getElementsByClassName('login');
 const $loginForm = document.getElementById('loginForm');
 const $conditionMessage = document.querySelectorAll('.condition-message');
 const $passwordEye = document.getElementById('passwordEye');
 const $checkPasswordEye = document.getElementById('checkPasswordEye');
-const pattern = /^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$/;
+const PATTERN = /^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$/;
 
 const pass = [false, false, false, false];
 
 // 인풋 입력값있는지 체크
 function inputCheck(e) {
   const $target = e.target;
-  const $errorMesaage = $target.nextElementSibling;
-  if ($target.value === '') {
-    $target.classList.add('error2');
-    $errorMesaage.classList.add('waring');
-  } else {
-    $target.classList.remove('error2');
-    $errorMesaage.classList.remove('waring');
-  }
+  const $message = $target.nextElementSibling;
+  const isValid = $target.value.trim() === '';
+  $target.classList.toggle('error2', isValid);
+  $message.classList.toggle('waring2', isValid);
 }
 
 // 이메일 정규식 체크
-function emailCheck() {
-  if ($email.value === '') {
-    $conditionMessage[0].classList.remove('waring');
-    $input[0].classList.remove('error');
-  } else if (!pattern.test($email.value)) {
-    $conditionMessage[0].classList.add('waring');
-    $input[0].classList.add('error');
-  } else {
-    $conditionMessage[0].classList.remove('waring');
-    $input[0].classList.remove('error');
-  }
+function emailCheck(e) {
+  if (e.target.id !== 'email') return;
+  const $target = e.target;
+  const $message = $conditionMessage[0];
+  const isValid = PATTERN.test($email.value) || $target.value.length === 0;
+  $target.classList.toggle('error', !isValid);
+  $message.classList.toggle('waring', !isValid);
 }
 
-// 비밀번호 8글자 이상인지 체크
+// // 비밀번호 8글자 이상인지 체크
 function passwordCheck(e) {
-  if (e.target.id === 'password') {
-    const $target = e.target;
-    const $Message = $target.nextElementSibling.nextElementSibling;
-    if ($target.value.length < 8 && $target.value.length > 0) {
-      $Message.classList.add('waring');
-      $target.classList.add('error');
-    } else {
-      $Message.classList.remove('waring');
-      $target.classList.remove('error');
-    }
-  }
+  if (e.target.id !== 'password') return;
+  const $target = e.target;
+  const $message = $target.nextElementSibling.nextElementSibling;
+  const isValid = $target.value.length >= 8 || $target.value.length === 0;
+  $target.classList.toggle('error', !isValid);
+  $message.classList.toggle('waring', !isValid);
 }
+
 // 비밀번호 재확인 체크
-function ReCheckPassword() {
-  if ($password.value !== $checkPassword.value) {
-    $conditionMessage[2].classList.add('waring');
-    $input[3].classList.add('error');
-  } else {
-    $conditionMessage[2].classList.remove('waring');
-    $input[3].classList.remove('error');
-  }
+function ReCheckPassword(e) {
+  if (e.target.id !== 'checkPassword') return;
+  const $target = e.target;
+  const $message = $conditionMessage[2];
+  const isValid = $password.value !== $checkPassword.value;
+  $target.classList.toggle('error', isValid);
+  $message.classList.toggle('waring', isValid);
 }
 
 // 조건 충족했는지 체크
 function meetsCondition() {
   for (let i = 0; i < $input.length; i++) {
+    const isError = $input[i].classList.contains('error') || $input[i].classList.contains('error2');
     if ($input[i].value !== '') {
-      if ($input[i].classList.contains('error') || $input[i].classList.contains('error2')) {
-        pass.splice(i, 1, false);
-      } else {
-        pass.splice(i, 1, true);
-      }
+      pass.splice(i, 1, isError ? false : true);
     }
   }
 }
 
 // 모든 조건을 충족했는지 체크 후 충족했다면 버튼 활성화
 function meetAallConditions() {
-  if ($input.length === 2) {
-    const loginPass = pass.slice(0, 2);
-    if (loginPass.every((value) => value === true)) {
-      $loginBtn[0].classList.add('accept');
-    } else {
-      $loginBtn[0].classList.remove('accept');
-    }
-  } else if (pass.every((value) => value === true)) {
-    $loginBtn[0].classList.add('accept');
-  } else {
-    $loginBtn[0].classList.remove('accept');
-  }
+  const isPass = $input.length === 2 ? pass.slice(0, 2) : pass;
+  const isValid = isPass.every((value) => value === true);
+  $loginBtn[0].classList.toggle('accept', isValid);
 }
 
 // 포커스 아웃 시에 함수들을 실행하여 조건 확인
 $loginForm.addEventListener('focusout', (e) => {
-  emailCheck();
+  emailCheck(e);
   passwordCheck(e);
   if ($input.length === 4) {
-    ReCheckPassword();
+    ReCheckPassword(e);
   }
   inputCheck(e);
   meetsCondition();
@@ -114,11 +85,7 @@ $loginForm.addEventListener('submit', (e) => {
   meetsCondition();
   meetAallConditions();
   if ($loginBtn[0].classList.contains('accept')) {
-    if ($input.length === 2) {
-      window.location.href = '/items';
-    } else {
-      window.location.href = '/signin';
-    }
+    $input.length === 2 ? (window.location.href = '/items') : (window.location.href = '/signin');
   }
 });
 
@@ -132,6 +99,7 @@ $passwordEye.addEventListener('click', () => {
     $passwordEye.src = 'image/icon/btn_icon/visibilty_on.png';
   }
 });
+
 $checkPasswordEye.addEventListener('click', () => {
   if ($checkPassword.type === 'text') {
     $checkPassword.type = 'password';
